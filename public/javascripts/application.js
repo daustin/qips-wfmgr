4026896f424db593d07db46b2ab8d437869f0401
David Austin <daustin@meath.gcrc.upenn.edu>
2010-06-14 14:57:58 -0400

    Added jrails files and altered nested forms code to be compatible with jquery.  now all javascripts are working

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,14 +4,12 @@
 //these support the nested forms concept from ryanb
 
 function remove_fields(link) {
-  $(link).previous("input[type=hidden]").value = "1";
-  $(link).up(".fields").hide();
+  $(link).prev("input[type=hidden]").val("1");
+  $(link).closest(".fields").hide();
 }
 
 function add_fields(link, association, content) {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + association, "g")
-  $(link).up().insert({
-    before: content.replace(regexp, new_id)
-  });
+  $(link).parent().before(content.replace(regexp, new_id));
 }
